00f6a73ed27cdcb9e98dd65101ed74df54c48c5e
Pulkit-1012 <userongrid@Admins-MacBook-Pro-4.local>
2025-01-30 10:08:39 +0530

    Initial commit

---
+++ src/main/java/in/ongrid/b2cverification/B2cverificationApplication.java
@@ -0,0 +1,14 @@
+
+package in.ongrid.b2cverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2cverificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2cverificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ongrid/b2cverification/B2cverificationApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ongrid.b2cverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2cverificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
